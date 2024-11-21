045d532ef7c225c27361243f316ab0d17d63d411
Spencer Miller <smiller@coti.io>
2024-11-21 11:45:14 +0200

    add type hinting

--- coti/types.py
+++ coti/types.py
@@ -1,13 +1,20 @@
-from typing import Dict, List, TypeAlias, Union
-
-ItBool: TypeAlias = Dict[str, Union[int, bytes]] # { 'ciphertext': int, 'signature': bytes }
-
-ItUint: TypeAlias = Dict[str, Union[int, bytes]] # { 'ciphertext': int, 'signature': bytes }
-
-ItString: TypeAlias = Dict[str, Union[Dict[str, List[int]], List[bytes]]] # { 'ciphertext': { 'value': List[int] }, 'signature': List[bytes] }
+from typing import List, TypeAlias, TypedDict
 
 CtBool: TypeAlias = int
 
 CtUint: TypeAlias = int
 
-CtString: TypeAlias = Dict[str, List[int]] # { 'value': List[int] }
+class CtString(TypedDict):
+    value: List[int]
+
+class ItBool(TypedDict):
+    ciphertext: int
+    signature: bytes
+
+class ItUint(TypedDict):
+    ciphertext: int
+    signature: bytes
+
+class ItString(TypedDict):
+    ciphertext: CtString
+    signature: List[bytes]
