95d46fbe871ea7a284091abb56003b925241b535
Spencer Miller <smiller@coti.io>
2024-12-11 11:49:37 +0200

    update readme

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup
 
+with open("./README.md") as readme:
+    long_description = readme.read()
+
 setup(
     name='coti_sdk',
     description='COTI SDK for Privacy Preserving Network',
-    version='v0.1.14',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='0.0.0',
     license='Apache2.0',
-    author="gmesika-coti@COTI",
-    author_email='support@coti.io',
+    author="COTI Development",
+    author_email='dev@coti.io',
     package_dir={'': '.'},
     url='https://github.com/coti-io/coti-sdk-python',
-    keywords='COTI SDK Privacy',
+    keywords=["coti", "privacy", "ethereum", "blockchain", "web3", "garbled-circuits", "l2", "on-chain-compute"],
     install_requires=[
         'pycryptodome==3.19.0', 'cryptography==3.4.8', 'eth-keys>=0.4.0', 'eth-account>=0.13.1'
     ],
